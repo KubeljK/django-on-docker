e08498564f29a2c22851fb2108ceb1cbfa4bbb97
klemen <klemen.kubelj@gmail.com>
2020-03-29 22:51:44 +0200

    Separate collectstatic file

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "storage/assets")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "storage/static"),
+]
 
-MEDIA_URL = "/mediafiles/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "storage/media")
